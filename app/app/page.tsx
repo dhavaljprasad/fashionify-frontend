280a1d620c318b056cd08adedf963361ab24314a
dhavaljprasad <dhavaljprasad@gmail.com>
2026-04-28 14:56:22 +0530

    feat: just made the base landing page after login

---
+++ app/app/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+function page() {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-4 bg-background-primary px-4 sm:px-16"></div>
+  )
+}
+
+export default page
