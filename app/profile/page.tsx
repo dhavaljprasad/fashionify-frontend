f9c19be54120366205310e7dfaf18f6a3c6e3bbd
dhavaljprasad <dhavaljprasad@gmail.com>
2026-05-02 02:52:16 +0530

    feat: init profile page just for routing though header and side bar

---
+++ app/profile/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+function page() {
+  return (
+    <div className="relative flex h-screen w-full flex-col items-center justify-start gap-4 bg-background-primary px-4 sm:px-16">
+      {/* <AppPageHeader /> */}
+    </div>
+  )
+}
+
+export default page
